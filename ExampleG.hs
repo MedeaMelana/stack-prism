fd26907d30414f9a907c68d2c44e30bff310ccca
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2013-05-03 18:38:57 +0300

    Gave aap' a *very* generic type and renamed it to giso.

--- ExampleG.hs
+++ ExampleG.hs
@@ -15,20 +15,17 @@ import GHC.Generics
 data Aap = Aap deriving Generic
 
 aap :: Iso t (Aap :- t)
-aap = Iso f g
-  where
-    f t = Just (Aap :- t)
-    g (Aap :- t) = Just t
+aap = giso
 
-aap' :: forall t. Iso t (Aap :- t)
-aap' = Iso f g
+giso :: forall a t. (Generic a, MkIso (Rep a)) => Iso (IsoLhs (Rep a) t) (a :- t)
+giso = Iso f g
   where
-    f :: forall a x t. (Generic a, MkIso (Rep a)) => IsoLhs (Rep a) x t -> Maybe (a :- t)
-    --f :: forall x t'. IsoLhs (Rep Aap) x t' -> Maybe (Aap :- t')
+    f :: forall a t'. (Generic a, MkIso (Rep a)) =>
+          IsoLhs (Rep a) t' -> Maybe (a :- t')
     f t = Just (mapHead to (mkR t))
 
-    g :: forall x. (Aap :- t) -> Maybe (IsoLhs (Rep Aap) x t)
-    --g :: forall a x. Generic a => (a :- t)
+    g :: forall a t. (Generic a, MkIso (Rep a)) =>
+          (a :- t) -> Maybe (IsoLhs (Rep a) t)
     g (x :- t) = Just (mkL (from x :- t))
 
 
@@ -38,17 +35,17 @@ aap' = Iso f g
 
 
 class MkIso (f :: * -> *) where
-  type IsoLhs (f :: * -> *) (p :: *) (t :: *) :: *
-  mkR :: IsoLhs f p t -> (f p :- t)
-  mkL :: (f p :- t) -> IsoLhs f p t
+  type IsoLhs (f :: * -> *) (t :: *) :: *
+  mkR :: forall p t. IsoLhs f t -> (f p :- t)
+  mkL :: forall p t. (f p :- t) -> IsoLhs f t
 
 instance MkIso U1 where
-  type IsoLhs U1 p t = t
+  type IsoLhs U1 t = t
   mkR t         = U1 :- t
   mkL (U1 :- t) = t
 
 instance MkIso f => MkIso (M1 i c f) where
-  type IsoLhs (M1 i c f) p t = IsoLhs f p t
+  type IsoLhs (M1 i c f) t = IsoLhs f t
   mkR = mapHead M1 . mkR
   mkL = mkL . mapHead unM1
 
