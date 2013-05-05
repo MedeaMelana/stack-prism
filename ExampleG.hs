9dde84e1b097e980f42015a0f5d710569be48992
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2013-05-05 12:09:00 +0300

    MkIsoList :+:

--- ExampleG.hs
+++ ExampleG.hs
@@ -28,37 +28,55 @@ noot = giso
 data Mies = Mies Int Char () deriving (Generic, Show)
 
 --mies :: forall t. Iso (Int :- Char :- () :- t) (Mies :- t)
-IsoList (Z mies) = wim (Mies 0 'c' ())
+IsoList (Z mies) = mkIsoList (Mies 0 'c' ())
 
---true :: Iso t (Bool :- t)
---true = giso
+IsoList (Z false' :& Z true') = mkIsoList True
 
 
-wim :: a -> (Generic a, MkIsoList (Rep a)) => IsoList (Rep a) a
-wim _ = mkIsoList to (Just . from)
+mkIsoList :: a -> (Generic a, MkIsoList (Rep a)) => IsoList (Rep a) a
+mkIsoList _ = mkIsoList' to (Just . from)
 
 
 class MkIsoList (f :: * -> *) where
   data IsoList (f :: * -> *) (a :: *)
-  mkIsoList :: (f p -> a) -> (a -> Maybe (f q)) -> IsoList f a
+  mkIsoList' :: (f p -> a) -> (a -> Maybe (f q)) -> IsoList f a
 
 
 instance MkIsoList f => MkIsoList (M1 D c f) where
   data IsoList (M1 D c f) a = IsoList (IsoList f a)
-  mkIsoList f' g' = IsoList (mkIsoList (f' . M1) (fmap unM1 . g'))
+  mkIsoList' f' g' = IsoList (mkIsoList' (f' . M1) (fmap unM1 . g'))
+
+
+instance (MkIsoList f, MkIsoList g) => MkIsoList (f :+: g) where
+  data IsoList (f :+: g) a = IsoList f a :& IsoList g a
+  mkIsoList' f' g' = f f' g' :& g f' g'
+    where
+      f :: forall a p q. ((f :+: g) p -> a) -> (a -> Maybe ((f :+: g) q)) -> IsoList f a
+      f _f' _g' = mkIsoList' (\fp -> _f' (L1 fp)) (matchL _g')
+      g :: forall a p q. ((f :+: g) p -> a) -> (a -> Maybe ((f :+: g) q)) -> IsoList g a
+      g _f' _g' = mkIsoList' (\gp -> _f' (R1 gp)) (matchR _g')
+
+      matchL :: (a -> Maybe ((f :+: g) q)) -> a -> Maybe (f q)
+      matchL _g' a = case _g' a of
+        Just (L1 f) -> Just f
+        _ -> Nothing
+
+      matchR :: (a -> Maybe ((f :+: g) q)) -> a -> Maybe (g q)
+      matchR _g' a = case _g' a of
+        Just (R1 g) -> Just g
+        _ -> Nothing
 
 
 instance MkIso f => MkIsoList (M1 C c f) where
 
   data IsoList (M1 C c f) a = Z (forall t. Iso (IsoLhs f t) (a :- t))
 
-  mkIsoList f' g' = Z (Iso (f f') (g g'))
+  mkIsoList' f' g' = Z (Iso (f f') (g g'))
     where
       f :: forall a p t. (M1 C c f p -> a) -> IsoLhs f t -> Maybe (a :- t)
       f _f' lhs = Just (mapHead (_f' . M1) (mkR lhs))
       g :: forall a p t. (a -> Maybe (M1 C c f p)) -> (a :- t) -> Maybe (IsoLhs f t)
       g _g' (a :- t) = fmap (mkL . (:- t) . unM1) (_g' a)
-
 
 
 -- Deriving types and conversions for single constructors
