9325938284fe5c79ca7641836dec817d639d2b30
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2013-05-06 17:45:26 +0300

    Removed Semigroup import

--- Data/Iso.hs
+++ Data/Iso.hs
@@ -3,12 +3,10 @@ module Data.Iso (
 
   module Data.Iso.Core,
   module Data.Iso.Common,
-  module Data.Iso.TH,
-  module Data.Semigroup
+  module Data.Iso.TH
 
   ) where
 
 import Data.Iso.Core
 import Data.Iso.Common
 import Data.Iso.TH
-import Data.Semigroup
